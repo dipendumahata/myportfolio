316b42bb45b70abb30217a8bd9ad18263f893230
Dipendu Mahata <166703575+dipendumahata@users.noreply.github.com>
2025-06-12 20:23:50 +0530

    change resume

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/17-_UEQ3w-b5TMLxtVqvL7ziyUPJ_ZoVI/view?usp=sharing"
+            href="https://drive.google.com/file/d/1t9ZsZLLSPkXoctyIuh5EQUw5Rwoc_81g/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
